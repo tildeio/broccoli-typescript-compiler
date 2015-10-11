5daaa192e3e38499f41768746aadd66de9078104
Philip Bjorge <philipbjorge@gmail.com>
2015-10-11 09:54:36 -0700

    Busted tests, some notes

--- index.ts
+++ index.ts
@@ -25,28 +25,29 @@ class TypeScript extends Filter {
 	}
 
 	processString(contents: string, relativePath: string): string {
-		// TODO: Need to know when files are deleted to remove from fileRegistry...
-		// Do stale entries actually cause problems for the language service?
-
 		// TODO: Test this.
-		// TODO: Output errors
 		// TODO: Reemit files in our registry that had prior build errors
 
-		// the first time a file is built, it has no cache entry
 		if (this.fileRegistry.get(relativePath)) {
+			// We know about this file, but its contents have changed.
+			// We temporarily update our filter's cache entry with the new contents and a new "version"
 			this.fileRegistry.get(relativePath).contents = contents;
-			this.fileRegistry.get(relativePath).hash.key.mtime++; // Hack to trigger update since cache doesn't get updated until after processString
+			this.fileRegistry.get(relativePath).hash.key.mtime++;
 		} else {
+			// This is a new file to us, so we create a temporary cache entry for our language host.
+			// This entry is overwritten on returning from this function.
 			this.fileRegistry.set(relativePath, {
 				contents,
 				hash: { key: { mtime: 1337 } }
-			}); // temporary cache entry
+			});
 		}
 
 		const output = this.tsService.getEmitOutput(relativePath);
 		if (output.outputFiles.length > 1) {
 			throw new Error("More than one file was emitted on this change... Are you using const enums or internal modules?");
 		}
+
+		// TODO: Emit Errors
 
 		return output.outputFiles[0].text; // after we return, our cache entry is overwritten so we lose contents. could keep separate cahce but then have to worry about deleting it
 	}
