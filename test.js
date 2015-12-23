98327bfe78f6aef781c647be3aa191380aa64ef7
Stefan Penner <stefan.penner@gmail.com>
2015-12-22 16:24:50 -0800

    add tests for load-ts-config

--- test.js
+++ test.js
@@ -18,7 +18,6 @@ var expectations = path.join(__dirname, 'expectations');
 var typescript;
 
 describe('transpile TypeScript', function() {
-
   before(function() {
     typescript = makeTestHelper({
       subject: function(tree, options) {
@@ -28,7 +27,6 @@ describe('transpile TypeScript', function() {
     });
   });
 
-
   afterEach(function () {
     return cleanupBuilders();
   });
@@ -36,7 +34,9 @@ describe('transpile TypeScript', function() {
   it('uses tsconfig from options', function () {
     var tsconfigPath = path.join(__dirname, "fixtures", "tsconfig.json");
 
-    return typescript('files', {tsconfig: tsconfigPath}).then(function(results) {
+    return typescript('files', {
+      tsconfig: tsconfigPath
+    }).then(function(results) {
       var outputPath = results.directory;
 
       var output = fs.readFileSync(path.join(outputPath, 'fixtures.js')).toString();
@@ -45,4 +45,59 @@ describe('transpile TypeScript', function() {
       expect(output).to.eql(input);
     });
   });
+});
+
+describe('loadTSConfig', function() {
+  var loadTSConfig = require('./lib/load-ts-config');
+
+  it('throws on invalid input', function() {
+    expect(loadTSConfig).to.throw(/to be a string/);
+    expect(function() {
+      loadTSConfig(undefined);
+    }).to.throw(/to be a string/);
+
+    expect(function() {
+      loadTSConfig(null);
+    }).to.throw(/to be a string/);
+  });
+
+  it('throws for missing file', function() {
+    expect(function() {
+      loadTSConfig('nothing/here');
+    }).to.throw('Cannot load tsconfig.json from: `nothing/here`\nENOENT: no such file or directory, open \'nothing/here\'');
+  });
+
+  it('throws for empty file', function() {
+    expect(function() {
+      loadTSConfig('tests/fixtures/empty-ts-config.js');
+    }).to.throw('Cannot load tsconfig.json from: `tests/fixtures/empty-ts-config.js`\ntsconfig file cannot be empty');
+  });
+
+  it('throws for malformed file', function() {
+    expect(function() {
+      loadTSConfig('tests/fixtures/malformed-ts-config.js');
+    }).to.throw('Cannot load tsconfig.json from: `tests/fixtures/malformed-ts-config.js`\nFailed to parse file \'tests/fixtures/malformed-ts-config.js\': Unexpected end of input.');
+  });
+
+  it('loads blank', function() {
+    expect(loadTSConfig('tests/fixtures/basic-ts-config.js')).to.deep.eql({
+      noEmit: false
+    });
+  });
+
+  it('loads advanced', function() {
+    expect(loadTSConfig('tests/fixtures/more-advanced-ts-config.js')).to.deep.eql({
+      inlineSourceMap: true,
+      inlineSources: true,
+      mapRoot: 'tests/fixtures/packages',
+      moduleResolution: 2,
+      noEmit: false,
+      rootDir: 'tests/fixtures/packages',
+      target: 2
+    });
+  });
+
+
+
+
 });
