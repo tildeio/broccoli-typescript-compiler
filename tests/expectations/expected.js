2a5b182f7a187578441670ac70992c8139edf9b8
Edward Faulkner <ef@alum.mit.edu>
2016-06-21 12:41:36 -0400

    update test to follow compiler output

--- tests/expectations/expected.js
+++ tests/expectations/expected.js
@@ -1,3 +1,4 @@
+"use strict";
 var types_1 = require('./types');
 var Person = (function () {
     function Person(name) {
@@ -7,5 +8,5 @@ var Person = (function () {
         return this._name;
     };
     return Person;
-})();
+}());
 document.body.innerHTML = new types_1["default"]().greet(new Person("Godfrey"));
