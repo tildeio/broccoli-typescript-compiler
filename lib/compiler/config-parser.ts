735c92e0e743cf725ca697681913911b31f0ba84
Kris Selden <kris.selden@gmail.com>
2017-09-02 18:57:27 -0700

    use new ts.parseJsonSourceFileConfigFileContent when parsing config file

--- lib/compiler/config-parser.ts
+++ lib/compiler/config-parser.ts
@@ -1,21 +1,14 @@
-import {
-  Diagnostic,
-  findConfigFile,
-  ParseConfigHost,
-  ParsedCommandLine,
-  parseJsonConfigFileContent,
-  readConfigFile,
-} from "typescript";
+import * as ts from "typescript";
 import {getDirectoryPath } from "../fs/path-utils";
 import { CompilerOptionsConfig, Path, TypeScriptConfig } from "../interfaces";
 import createParseConfigHost from "./create-parse-config-host";
 import Input from "./input-io";
 
 export default class ConfigParser {
-  private host: ParseConfigHost;
+  private host: ts.ParseConfigHost;
 
   constructor(private projectPath: Path,
-              private rawConfig: CompilerOptionsConfig | undefined,
+              private rawConfig: TypeScriptConfig | undefined,
               private configFileName: string | undefined,
               private compilerOptions: CompilerOptionsConfig | undefined,
               workingPath: Path,
@@ -23,45 +16,74 @@ export default class ConfigParser {
     this.host = createParseConfigHost(workingPath, input);
   }
 
-  public parseConfig(): ParsedCommandLine {
-    const rawConfig: TypeScriptConfig = {};
+  public parseConfig(): ts.ParsedCommandLine {
+    const configFileName = this.resolveConfigFileName();
+    const basePath = this.getBasePath(configFileName);
+    const existingOptions = this.convertExistingOptions(basePath);
 
-    const projectPath = this.projectPath;
-    const host = this.host;
-    const errors: Diagnostic[] = [];
-    let configFileName: Path | undefined;
-    if (this.rawConfig !== undefined) {
-      Object.assign(rawConfig, this.rawConfig);
-    } else {
-      configFileName = this.resolveConfigFileName();
-      if (configFileName) {
-        const readResult = readConfigFile(configFileName, host.readFile);
-        if (readResult.error) {
-          errors.push(readResult.error);
-        } else {
-          Object.assign(rawConfig, readResult.config);
-        }
-      }
+    const result = this.parseConfigContent(configFileName, basePath, existingOptions.options);
+
+    if (existingOptions.errors.length > 0) {
+      result.errors = existingOptions.errors.concat(result.errors);
     }
-    const basePath = configFileName ? getDirectoryPath(configFileName) : projectPath;
-    if (this.compilerOptions) {
-      if (rawConfig.compilerOptions === undefined) {
-        rawConfig.compilerOptions = this.compilerOptions;
-      } else {
-        rawConfig.compilerOptions = Object.assign({}, rawConfig.compilerOptions, this.compilerOptions);
-      }
-    }
-    const result = parseJsonConfigFileContent(
-      rawConfig, host, basePath, undefined, configFileName);
-    result.errors = errors.concat(result.errors);
+
     if (result.options.noEmit === true) {
       result.options.noEmit = false;
     }
+
     return result;
   }
 
-  private resolveConfigFileName(): Path {
-    const { configFileName, projectPath, host } = this;
-    return findConfigFile(projectPath, host.fileExists, configFileName) as Path;
+  private resolveConfigFileName(): Path | undefined {
+    if (this.rawConfig !== undefined) {
+      return;
+    }
+    return ts.findConfigFile(
+      this.projectPath,
+      this.host.fileExists,
+      this.configFileName) as Path;
+  }
+
+  private getBasePath(configFilePath: Path | undefined) {
+    if (configFilePath === undefined) {
+      return this.projectPath;
+    }
+    return getDirectoryPath(configFilePath);
+  }
+
+  private convertExistingOptions(basePath: Path) {
+    const { compilerOptions } = this;
+    if (compilerOptions === undefined) {
+      return {
+        errors: [] as ts.Diagnostic[],
+        options: undefined,
+      };
+    }
+    return ts.convertCompilerOptionsFromJson(this.compilerOptions, basePath);
+  }
+
+  private readConfigSourceFile(configFilePath: Path | undefined): ts.JsonSourceFile | undefined {
+    if (configFilePath === undefined) {
+      return;
+    }
+    const configFileText = this.host.readFile(configFilePath);
+    if (configFileText === undefined) {
+      throw new Error(`File '${configFilePath}' not found.`);
+    }
+    return ts.parseJsonText(configFilePath, configFileText);
+  }
+
+  private parseConfigContent(
+    configFileName: Path | undefined,
+    basePath: Path,
+    existingOptions: ts.CompilerOptions | undefined,
+  ) {
+    const configSourceFile = this.readConfigSourceFile(configFileName);
+    if (configSourceFile === undefined) {
+      return ts.parseJsonConfigFileContent(
+        this.rawConfig || {}, this.host, basePath, existingOptions);
+    }
+    return ts.parseJsonSourceFileConfigFileContent(
+      configSourceFile, this.host, basePath, existingOptions, configFileName);
   }
 }
